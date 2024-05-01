04ba21b09458e124675772fab939ca956bbc659a
Jack English <jackjenglish@gmail.com>
2024-05-01 14:58:43 -0700

    update configs

--- readme.md
+++ readme.md
@@ -1,9 +1,9 @@
 # basically Promise.all but python.
 
 ```python
-from in_parallel.in_parallel import in_parallel
+import in_parallel
 
 # Your function and arguments here
-result = in_parallel(sample_function, args=[(1,), (2,)])
+result = in_parallel.all(sample_function, args=[(1,), (2,)])
 print(result)
 ```
--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='in_parallel_package',
+    name='in_parallel',
     version='0.1.0',
-    author='Your Name',
-    author_email='your.email@example.com',
+    author='Jack English',
+    author_email='jackjenglish@gmail.com',
     packages=find_packages(),
     install_requires=[
-        'tqdm',
-        'concurrent-futures; python_version<"3.2"'
+        'tqdm' 
     ],
     python_requires='>=3.6',
     description='A simple utility for running functions in parallel.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourgithub/in_parallel_package',  # Optional
+    url='https://github.com/jackjenglish/in_parallel'
 )
